16873165be8311b66f13a5ae2a20c8bace765c35
Emad Alharbi <40588388+E-Alharbi@users.noreply.github.com>
2022-03-16 11:07:56 +0000

    Update Script8.r

--- Scripts/Script8/Script8.r
+++ Scripts/Script8/Script8.r
@@ -38,7 +38,7 @@ keywords[["x_label"]] <- new("keyword",name="x_label",type="col", in_out="in", v
 
 keywords[["group_data"]] <- new("keyword",name="secondary_x_label",type="col", in_out="in", value="Structure_evaluation")
 
-keywords[["font_size"]] <- new("keyword",name="font_size",type="size", in_out="in", value="14")
+keywords[["font_size"]] <- new("keyword",name="font_size",type="size", in_out="in", value="8")
 
 
 
@@ -74,7 +74,7 @@ panel.border = element_blank(),
 panel.background = element_blank()) +scale_fill_manual(values="#0073C2FF")+ theme(legend.position = c(0.90, 0.62))+ theme(legend.title = element_blank())  +theme(panel.margin=unit(.05, "lines"),
 panel.border = element_rect(color = "black", fill = NA, size = 0.5),
 strip.background = element_rect(color = "black", size = 0.5))+     stat_ecdf(aes_(y=bquote(..y.. * .(max_yCom))),geom = "line") +
-scale_y_continuous(sec.axis=sec_axis(trans = ~./max_yCom, name="percentage",labels=scales::percent_format(suffix = "")))+ scale_x_continuous(breaks = seq(0, max_xCom, by = 1),expand = c(0.01,0)) +theme(text = element_text(size=8)) +scale_color_manual(name = "Y series", values = c("Cumulative percentage" = "black"))+theme(legend.key = element_rect(fill = NA))+theme(
+scale_y_continuous(sec.axis=sec_axis(trans = ~./max_yCom, name="percentage",labels=scales::percent_format(suffix = "")))+ scale_x_continuous(breaks = seq(0, max_xCom, by = 1),expand = c(0.01,0)) +theme(text = element_text(size=as.numeric(keywords[["font_size"]]@value))) +scale_color_manual(name = "Y series", values = c("Cumulative percentage" = "black"))+theme(legend.key = element_rect(fill = NA))+theme(
 legend.spacing.y = unit(0.001, 'cm'))+theme(
 legend.spacing.x = unit(0.07, 'cm'))+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
